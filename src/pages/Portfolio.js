7973c0a5c2f2d17e703a5a93438f9db95d9ee5bf
orcadehub <orcadehub2@gmail.com>
2024-12-11 14:33:21 +0530

    dataset

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -56,14 +56,14 @@ const Portfolio = () => {
             />
             <h3 className="project-title">{project.title}</h3>
             <p className="project-description">{project.description}</p>
-            <a
+            {/* <a
               href={project.link}
               target="_blank"
               rel="noopener noreferrer"
               className="project-link"
             >
               View Project
-            </a>
+            </a> */}
           </div>
         ))}
       </div>
